99cd474d67410f6ab35c9379b2e44509cc9e9d51
konrad <ksiborenko@gmail.com>
2021-03-13 08:48:33 +0100

    .

--- core/src/com/space/App.java
+++ core/src/com/space/App.java
@@ -4,9 +4,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.space.screens.LevelFinal;
-import com.space.screens.LevelNine;
-import com.space.screens.LevelSeven;
 import com.space.screens.WelcomeScreen;
 import com.space.utils.SaveData;
 
@@ -46,7 +43,7 @@ public class App extends Game {
         this.horn = Gdx.audio.newMusic(Gdx.files.internal("horn.mp3"));
         this.charge = Gdx.audio.newMusic(Gdx.files.internal("charge.mp3"));
 
-        this.setScreen(new LevelNine(this));
+        this.setScreen(new WelcomeScreen(this));
     }
 
     @Override
